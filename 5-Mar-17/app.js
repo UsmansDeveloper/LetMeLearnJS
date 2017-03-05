d8eb4d119c498df150b6e4e14464e99f911a0cf8
unknown <xyz@yahoo.com>
2017-03-05 22:29:40 +0500

    This is my FIrst Commit

---
+++ 5-Mar-17/app.js
@@ -0,0 +1,21 @@
+var a = +prompt ("Enter Any percentage.");
+if ( a >= 80 ){
+    alert("Your Grade is A+")
+}
+else if ( a >= 70) {
+    alert ("Your Grade is A")
+}
+
+else if ( a >= 60) {
+    alert ("Your Grade is B")
+}
+
+else if ( a >= 50) {
+    alert ("Your Grade is C")
+}
+else if ( a >= 40) {
+    alert ("Your Grade is D")
+}
+else {
+    alert ("You Are Failed!!")
+}
