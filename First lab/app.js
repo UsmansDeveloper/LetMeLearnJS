12269e698281985587835ab7203b7ae7c8539d6c
Usman <followmuhammedu@gmail.com>
2017-03-09 16:48:37 +0500

    First lab of JavaScript

---
+++ First lab/app.js
@@ -0,0 +1,43 @@
+// first task
+
+// var firstNum = +prompt("Enter First Number");
+// var secondNum= +prompt("Enter FIrst Number");
+// var operation= prompt("Enter any operation");
+
+// if (operation === "+") {
+// document.write( firstNum + secondNum);
+// }
+// else if (operation === "-") {
+
+// document.write( firstNum - secondNum);
+// }
+// else if (operation === "*") {
+// document.write( firstNum * secondNum);
+// }
+// else if (operation === "/") {
+// document.write( firstNum / secondNum);
+// }
+// else {
+// document.write("Invalid Entry");
+// }
+
+
+//  second task
+// var table = +prompt("Enter Any Number");
+
+// document.write ("<br/>" + table + " x 2 = "+ table*2);
+// document.write ("<br/>" + table + " x 3 = "+ table*3);
+// document.write ("<br/>" + table + " x 4 = "+ table*4);
+// document.write ("<br/>" + table + " x 5 = "+ table*5);
+// document.write ("<br/>" + table + " x 6 = "+ table*6);
+// document.write ("<br/>" + table + " x 7 = "+ table*7);
+// document.write ("<br/>" + table + " x 8 = "+ table*8);
+// document.write ("<br/>" + table + " x 9 = "+ table*9);
+// document.write ("<br/>" + table + " x 10 = "+ table*10);
+
+// third task
+var name =prompt("Enter Your Name");
+    email =prompt("Enter Your Email");
+
+document.write("<br/>" + "Name: " + name);
+document.write("<br/>" + "Email: " + email);
