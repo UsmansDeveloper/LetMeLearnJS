3ac3cf482de7d5489e397b69bcb1af69fdea2723
Usman <followmuhammedu@gmail.com>
2017-03-16 16:46:16 +0500

    Second Lab

---
+++ Second lab/app.js
@@ -0,0 +1,105 @@
+// first task
+
+// var per = +prompt("Enter Any Percentage?");
+
+// if (per >= 80 && per < 100 )
+// {
+//     alert("A+")
+// }
+// else if (per >= 70 && per < 80 )
+// {
+//     alert("A")
+// }
+// else if (per >= 60 && per < 70)
+// {
+//     alert("B")
+// }
+// else if (per >= 50 && per < 60 )
+// {
+//     alert("C")
+// }
+// else if (per >= 40 && per < 50 )
+// {
+//     alert("D")
+// }
+// else if (per >= 33 && per < 40 )
+// {
+//     alert("E")
+// }
+// else if (per < 33 )
+// {
+//     alert("You are Failed!")
+// }
+// if (per > 100 )
+// {
+//     alert("Invalid Percentage")
+// }
+// else {
+//     alert("Enter Valid Percentage")
+// }
+
+            // second task
+
+
+var test = prompt("Enter Your 1st course");
+var score = +prompt("Enter Your Gained Marks");
+
+if ( test === "HTML" && score > 50)
+{
+    console.log("Your HTML is clear")
+document.write("Your HTML is clear")
+}
+else {
+    console.log("You are failed")
+document.write("You are failed")
+}
+
+var test = prompt("Enter Your 2nd course");
+var score = +prompt("Enter Your Gained Marks");
+
+if ( test === "CSS" && score > 50)
+{
+    console.log("<br />" + "Your CSS is clear")
+document.write("<br />" + "Your CSS is clear")
+}
+else {
+    console.log("You are failed")
+document.write("You are failed")
+}
+
+var test = prompt("Enter Your 3rd course");
+var score = +prompt("Enter Your Gained Marks");
+
+if ( test === "JS" && score > 50)
+{
+    console.log("<br />" + "Your JS is clear")
+document.write("<br />" + "Your JS is clear")
+}
+else {
+    console.log("You are failed")
+document.write("You are failed")
+}
+
+
+// var test = "HTML"
+// var score = 30;
+
+// if ( test === "ice" || score == 30)
+// {
+//     console.log("Congrats")
+// }
+
+
+        // thrird task
+
+// var test = "CSS";
+// var num = 34;
+
+// if (test==="eeCSS" && num === 34 || test!=="CSS" || num < 12)
+// {
+//     console.log("Congrats")
+// }
+
+// var username ="Usman";
+//  var result= username ==="Usxcbxcbman" ? "Success" : "Fail";
+// console.log(result);
