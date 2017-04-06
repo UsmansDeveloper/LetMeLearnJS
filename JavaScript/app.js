9eacf8381f5b2fae1d28534a1394ffb77c001939
Usman <followmuhammedu@gmail.com>
2017-04-06 16:35:16 +0500

    Fourth Lab

---
+++ JavaScript/app.js
@@ -0,0 +1,25 @@
+        // document.write("<h2>" + "Question#1" + "</h2>");
+        // var stringToCome = "Hello World!";
+        // for (var i = 1; i < 6; i++) {
+        //     document.write(i + " " + stringToCome + "<br />");
+        // }
+        // document.write("<br />")
+
+        // document.write("<h2>" + "Question#2" + "</h2>");
+        // for (var i = 1; i <= 10; i++) {
+        //     document.write("<b>" + i + "</b>" + "<br />");
+        // }
+        // document.write("<br />")
+
+        // document.write("<h2>" + "Question#3" + "</h2>");
+        // var numtoPrint = +prompt("Enter The Table # You Want To Print!");
+        // var tableRange = +prompt("Enter The Range You Want The Table To Go Till!")
+
+        // for (i = 1; i <= tableRange; i++) {
+        //     document.write(numtoPrint + " x " + i + " = " + numtoPrint * i + "<br />");
+        // }
+        document.write("<br />")
+
+        document.write("<h2>" + "Question#4" + "</h2>");
+
+        var A = ["Nokia", "Samsung", "Apple", "Sony", "Huawei"];
